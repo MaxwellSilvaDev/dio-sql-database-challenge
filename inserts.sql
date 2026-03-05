544842519a73710b70f9ce267b2919d829211582
MaxwellSilvaDev <maxwellanabil78@gmail.com>
2026-03-05 02:40:49 -0300

    Create inserts.sql

---
+++ inserts.sql
@@ -0,0 +1,8 @@
+INSERT INTO Cliente(nome, telefone, cpf,cnpj, endereco, tipo_cliente)
+VALUES ('Maxwell', '62999999999', 00011155555,NULL,'Bairro Pedrinhas', 'PF');
+
+INSERT INTO Cliente(nome, telefone, cpf, cnpj, endereco, tipo_cliente)
+VALUES ('Edinelson', '62999988888', 00054422331,NULL,'Bairro Cristalzinho', 'PF');
+
+INSERT INTO Cliente(nome, telefone, cpf, cnpj, endereco, tipo_cliente)
+VALUES ('Dherick', '25664154125', NULL, '01253647825143','Bairro Vista Grossa','PJ');
